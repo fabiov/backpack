4a986f98d239a154e3a509a2ec35d77f549c21d8
Fabio Ventura <foravatenubi@gmail.com>
2024-02-29 17:31:55 +0100

    Add category CRUD

---
+++ app/Http/Controllers/Admin/CategoryCrudController.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Requests\CategoryRequest;
+use App\Models\Category;
+use Backpack\CRUD\app\Http\Controllers\CrudController;
+use Backpack\CRUD\app\Library\CrudPanel\CrudPanelFacade as CRUD;
+
+/**
+ * Class CategoryCrudController
+ * @package App\Http\Controllers\Admin
+ * @property-read \Backpack\CRUD\app\Library\CrudPanel\CrudPanel $crud
+ */
+class CategoryCrudController extends CrudController
+{
+    use \Backpack\CRUD\app\Http\Controllers\Operations\ListOperation;
+    use \Backpack\CRUD\app\Http\Controllers\Operations\CreateOperation;
+    use \Backpack\CRUD\app\Http\Controllers\Operations\UpdateOperation;
+    use \Backpack\CRUD\app\Http\Controllers\Operations\DeleteOperation;
+    use \Backpack\CRUD\app\Http\Controllers\Operations\ShowOperation;
+
+    public function setup(): void
+    {
+        CRUD::setModel(\App\Models\Category::class);
+        CRUD::setRoute(config('backpack.base.route_prefix') . '/category');
+        CRUD::setEntityNameStrings('category', 'categories');
+    }
+
+    /**
+     * @see https://backpackforlaravel.com/docs/crud-operation-list-entries
+     */
+    protected function setupListOperation(): void
+    {
+        CRUD::setFromDb(); // set columns from db columns.
+    }
+
+    protected function setupCreateOperation(): void
+    {
+        CRUD::setValidation(CategoryRequest::class);
+
+        CRUD::field('name')
+            ->type('text');
+        CRUD::field('active')
+            ->type('checkbox');
+
+        Category::creating(function($entry) {
+            $entry->user_id = backpack_user()->id;
+        });
+    }
+
+    protected function setupUpdateOperation(): void
+    {
+        CRUD::setValidation(CategoryRequest::class);
+
+        CRUD::field('name')
+            ->type('text');
+        CRUD::field('active')
+            ->type('checkbox');
+    }
+}
--- resources/views/vendor/backpack/ui/inc/menu_items.blade.php
+++ resources/views/vendor/backpack/ui/inc/menu_items.blade.php
@@ -2,3 +2,4 @@
 <li class="nav-item"><a class="nav-link" href="{{ backpack_url('dashboard') }}"><i class="la la-home nav-icon"></i> {{ trans('backpack::base.dashboard') }}</a></li>
 
 <x-backpack::menu-item title="Accounts" icon="la la-question" :link="backpack_url('account')" />
+<x-backpack::menu-item title="Categories" icon="la la-question" :link="backpack_url('category')" />
